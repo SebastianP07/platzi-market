57274e871d372abbee1b9effb5f37ef7090a40a1
Sebastian Pinto Becerra <sebastianpintobecerra7@gmail.com>
2021-02-22 17:24:49 -0500

    Generar un JWT

---
+++ src/main/java/com/platzi/market/web/security/JWTUtil.java
@@ -0,0 +1,17 @@
+package com.platzi.market.web.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Date;
+
+public class JWTUtil {
+    private static final String KEY = "pl4tz1"; // Clave para firmar el JWT
+
+    public String generateToken(UserDetails userDetails) {
+        return Jwts.builder().setSubject(userDetails.getUsername()).setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10hrs
+                .signWith(SignatureAlgorithm.HS256, KEY).compact();
+    }
+}
