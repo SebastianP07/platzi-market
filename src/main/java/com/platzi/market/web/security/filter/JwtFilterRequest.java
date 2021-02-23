63cc24543a00ea222cddb46c8e30a6d41a217c8b
Sebastian Pinto Becerra <sebastianpintobecerra7@gmail.com>
2021-02-22 19:25:14 -0500

    Autorizacion con JWT

---
+++ src/main/java/com/platzi/market/web/security/filter/JwtFilterRequest.java
@@ -0,0 +1,54 @@
+package com.platzi.market.web.security.filter;
+
+import com.platzi.market.domain.service.PlatziUserDetailsService;
+import com.platzi.market.web.security.JWTUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtFilterRequest extends OncePerRequestFilter {
+
+    @Autowired
+    private JWTUtil jwtUtil;
+
+    @Autowired
+    private PlatziUserDetailsService platziUserDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authorizationHeader = request.getHeader("Authorization");
+
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer")) {
+            String jwt = authorizationHeader.substring(7); // Descontamos el Bearer y el espcio
+            String username = jwtUtil.extractUsername(jwt); // Verifico el usuario de ese JWT
+
+            // Verifico que no exista una autenticacion para este usuario
+            // SecurityContextHolder.getContext().getAuthentication() == null :
+            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                // Verificamos el usuario segun el usuario que llego en el JWT
+                UserDetails userDetails = platziUserDetailsService.loadUserByUsername(username);
+
+                if (jwtUtil.validateToken(jwt, userDetails)) {
+                    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                    // Extraemos los datos de navegacion navegador, horario, ETC
+                    authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    // Asignamos la autenticacion para no hacer la validacion en proximas peticiones
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                }
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
